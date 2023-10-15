b66aa1b7faf4dfea9164d3e6a6645c317766909a
Emoji <studyemoji@mail.ru>
2023-10-15 15:14:18 +0300

    Initial commit

---
+++ src/test/java/rut/miit/mongoapp/MongoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rut.miit.mongoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
